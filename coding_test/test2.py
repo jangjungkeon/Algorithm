d46df00c415e29e9022828fb4db6561d3e4127d4
jangjungkeon <43978802+jangjungkeon@users.noreply.github.com>
2021-02-26 14:28:44 +0900

    Delete coding_test directory
    새출발

--- coding_test/test2.py
+++
@@ -1,8 +0,0 @@
-lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-
-answer = [[1 for _ in range(len(lock))] for _ in range(len(lock))]
-
-for i in range(len(lock)):
-    for j in range(len(lock)):
-        print(answer[i][j])
-        print(answer)
