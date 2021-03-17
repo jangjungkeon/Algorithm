fdf5ba41319b1524e7aef52f764d40ca147cfe3e
jangjungkeon <jkit13@naver.com>
2021-03-17 14:12:32 +0900

    test.py ignore 및 백준 1707번

--- baekjoon_study/DFS&BFS/baekjoon_1707.py
+++ baekjoon_study/DFS&BFS/baekjoon_1707.py
@@ -2,50 +2,67 @@ import sys
 from collections import deque
 
 
-# 그래프 만들기
-def makeGrape(grape):
-    t_grape = {}
-    for i in grape:
-        if i[0] not in t_grape:
-            t_grape[i[0]] = {i[1]}
-        else:
-            t_grape[i[0]].add(i[1])
-        if i[1] not in t_grape:
-            t_grape[i[1]] = {i[0]}
-        else:
-            t_grape[i[1]].add(i[0])
-    return t_grape
-
-
-def bfs(V, E, grape):
+def bfs(V, E, t_grape, s):
     if V == 1 and E == 1:
-        return print('YES')
+        return True
     q = deque()
-    t_grape = makeGrape(grape)
-    q.append(grape[0][0])
-    chk = [0 for _ in range(V+1)]
-    chk[grape[0][0]] = 1
+    q.append(s)
+    chk[s] = 1
 
     # print(t_grape)
     while q:
         a = q.popleft()
-        b = t_grape[a]
-        for i in b:
-            # print(chk)
-            if chk[i] == 0:
-                chk[i] = chk[a] + 1
-                q.append(i)
-                continue
-            if (chk[i] % 2) == (chk[a] % 2):
-                return print("NO")
-    return print("YES")
+        if a in t_grape:
+            b = t_grape[a]
+            for i in b:
+                # print(chk)
+                if chk[i] == 0:
+                    chk[i] = -chk[a]
+                    q.append(i)
+                    continue
+                if chk[i] == chk[a]:
+                    return False
+    return True
 
 
 if __name__ == "__main__":
     T = int(sys.stdin.readline().strip())
     for _ in range(T):
         V, E = map(int, sys.stdin.readline().split())
-        grape = []
+        grape = {}
+        # grape1 = [[] for _ in range(V+1)]
+        chk = [0 for _ in range(V + 1)]
+        isTrue = True
         for _ in range(E):
-            grape.append(list(map(int, sys.stdin.readline().split())))
-        bfs(V, E, grape)
+            m, n = map(int, sys.stdin.readline().split())
+            if m not in grape:
+                grape[m] = {n}
+            else:
+                grape[m].add(n)
+            if n not in grape:
+                grape[n] = {m}
+            else:
+                grape[n].add(m)
+            # grape1[m].append(n)
+            # grape1[n].append(m)
+        for s in range(1, V+1):
+            if chk[s] == 0:
+                if not bfs(V, E, grape, s):
+                    isTrue = False
+                    break
+        print("YES" if isTrue else "NO")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
